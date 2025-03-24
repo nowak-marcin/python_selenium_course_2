43915669ea2d0b1521c049f1c8880a19e4c590b5
nowakmarcin2204 <nowakmarcin2204@gmail.com>
2025-03-24 13:55:54 +0100

    timetable_objects autouse

--- tests/test_timetable.py
+++ tests/test_timetable.py
@@ -3,30 +3,36 @@ import pytest
 
 from src.pages.timetablePage import TimetablePage
 
-
-station_from = 'Szczecin Główny'
-station_to = 'Gryfino'
-hour = '1500'
-train_type = 'regional and tlk trains'
+# @pytest.fixture(autouse=True):
+# - function with this fixture (decorator) will be used in all tests in class,
+# - initialize objects without declaration and connection in each test separately.
+# - recognize 'self.driver' already exists into another class
 
 
 @pytest.mark.usefixtures('setup')
 class TestTimetable:
 
-    def test_tc_001(self):
+    @pytest.fixture(autouse=True)
+    def timetable_objects(self):
+        self.timetable = TimetablePage(self.driver)
 
-        timetable1 = TimetablePage(self.driver)
-        timetable1.input_from_station(station_from)
-        timetable1.input_to_station(station_to)
+    def test_tc_001(self):
+        station_from = 'Szczecin Główny'
+        station_to = 'Gryfino'
+        hour = '1500'
+        train_type = 'regional and tlk trains'
+
+        self.timetable.input_from_station(station_from)
+        self.timetable.input_to_station(station_to)
         time.sleep(5)
-        timetable1.select_tomorrow()
+        self.timetable.select_tomorrow()
         time.sleep(5)
-        timetable1.input_hour(hour)
+        self.timetable.input_hour(hour)
         time.sleep(10)
-        timetable1.select_no_transfer_option()
+        self.timetable.select_no_transfer_option()
         time.sleep(10)
-        timetable1.deselect_type_of_train(train_type)
-        timetable1.select_train_operators()
+        self.timetable.deselect_type_of_train(train_type)
+        self.timetable.select_train_operators()
         time.sleep(10)
-        timetable1.click_search_connection()
+        self.timetable.click_search_connection()
         time.sleep(10)
